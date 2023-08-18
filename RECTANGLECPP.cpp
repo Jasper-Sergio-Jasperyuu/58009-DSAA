cdc31a7cd3683dd78f55464d9b46597a2aee2850
Jasper-Sergio-Jasperyuu <123426437+Jasper-Sergio-Jasperyuu@users.noreply.github.com>
2023-08-18 11:52:22 +0800

    Create RECTANGLECPP.cpp

---
+++ RECTANGLECPP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b, c;
+  cout <<"RECTANGLE AREA CALCULATOR" << endl;
+  cout << "What is your first number: ";
+  cin >> a;
+  cout<<"What is your second number: ";
+  cin >> b;
+  c = a*b;
+  cout << "The area of your rectangle is: "<< c;
+    return 0;
+}
